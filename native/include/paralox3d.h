d3ca330d5f6410574522d98df1ef82f7afa9726f
N0one123 <ayaansharma1714@gmail.com>
2026-09-18 18:52:22 +0530

    Rename native API to Paralox3D

---
+++ native/include/paralox3d.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef _WIN32
+    #ifdef P3D_BUILD
+        #define P3D_API __declspec(dllexport)
+    #else
+        #define P3D_API __declspec(dllimport)
+    #endif
+#else
+    #define P3D_API
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct P3DEngine P3DEngine;
+
+P3D_API P3DEngine* p3d_engine_create(int width, int height, const char* title);
+P3D_API void p3d_engine_destroy(P3DEngine* engine);
+P3D_API uint32_t p3d_entity_create(P3DEngine* engine);
+P3D_API void p3d_entity_set_position(P3DEngine* engine, uint32_t entity, float x, float y, float z);
+P3D_API int p3d_engine_step(P3DEngine* engine);
+
+#ifdef __cplusplus
+}
+#endif
