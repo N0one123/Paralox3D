1e53f080b0b0199699daae1bacd5d48bf78110ce
N0one123 <ayaansharma1714@gmail.com>
2026-09-18 18:13:33 +0530

    Add first API example

---
+++ examples/hello.py
@@ -0,0 +1,11 @@
+from vertex3d import Engine, Entity
+
+engine = Engine(title="Vertex3D Hello", width=1280, height=720)
+
+cube = Entity(
+    engine,
+    model="cube",
+    position=(0.0, 0.0, 5.0),
+)
+
+engine.run()
