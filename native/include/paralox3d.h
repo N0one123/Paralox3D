cf821366354f27d65f1cc3538a56461c4cd24d27
N0one123 <ayaansharma1714@gmail.com>
2026-09-23 18:31:39 +0530

    Expose native keyboard input

--- native/include/paralox3d.h
+++ native/include/paralox3d.h
@@ -22,6 +22,7 @@ P3D_API P3DEngine* p3d_engine_create(int width, int height, const char* title);
 P3D_API void p3d_engine_destroy(P3DEngine* engine);
 P3D_API uint32_t p3d_entity_create(P3DEngine* engine);
 P3D_API void p3d_entity_set_position(P3DEngine* engine, uint32_t entity, float x, float y, float z);
+P3D_API int p3d_input_key_held(P3DEngine* engine, int key_code);
 P3D_API void p3d_engine_set_developer_overlay(P3DEngine* engine, int enabled, int object_count,
                                               float fps, const char* current_task, int warning_count);
 P3D_API int p3d_engine_diagnostics_clicked(P3DEngine* engine);
