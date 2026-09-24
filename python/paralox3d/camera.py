2ce9f81965539e17bc4d4ce70504663f4f14bf48
N0one123 <ayaansharma1714@gmail.com>
2026-09-24 17:10:23 +0530

    Add built-in camera API

---
+++ python/paralox3d/camera.py
@@ -0,0 +1,73 @@
+"""Simple built-in camera for Paralox3D."""
+
+from .math import Vec3
+
+
+class Camera:
+    """Default camera. Left-drag rotates; right-drag pans."""
+
+    def __init__(self):
+        self._position = Vec3()
+        self._rotation = Vec3()
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        self._position = Vec3(*value)
+
+    @property
+    def rotation(self):
+        return self._rotation
+
+    @rotation.setter
+    def rotation(self, value):
+        self._rotation = Vec3(*value)
+
+    @property
+    def x(self): return self._position.x
+    @x.setter
+    def x(self, value): self._position.x = float(value)
+
+    @property
+    def y(self): return self._position.y
+    @y.setter
+    def y(self, value): self._position.y = float(value)
+
+    @property
+    def z(self): return self._position.z
+    @z.setter
+    def z(self, value): self._position.z = float(value)
+
+    @property
+    def pitch(self): return self._rotation.x
+    @pitch.setter
+    def pitch(self, value): self._rotation.x = float(value)
+
+    @property
+    def yaw(self): return self._rotation.y
+    @yaw.setter
+    def yaw(self, value): self._rotation.y = float(value)
+
+    @property
+    def roll(self): return self._rotation.z
+    @roll.setter
+    def roll(self, value): self._rotation.z = float(value)
+
+    def move(self, x=0.0, y=0.0, z=0.0):
+        self.x += x
+        self.y += y
+        self.z += z
+
+    def look_at(self, target):
+        import math
+        dx = target.x - self.x
+        dy = target.y - self.y
+        dz = target.z - self.z
+        self.yaw = math.degrees(math.atan2(dx, dz))
+        self.pitch = -math.degrees(math.atan2(dy, math.hypot(dx, dz)))
+
+
+camera = Camera()
