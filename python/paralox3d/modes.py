ca40f83bea4fdb876d12168c5ce3452dc2ec2fcc
N0one123 <ayaansharma1714@gmail.com>
2026-09-24 17:10:13 +0530

    Add camera runtime mode

--- python/paralox3d/modes.py
+++ python/paralox3d/modes.py
@@ -3,6 +3,17 @@
 class _Modes:
     def __init__(self):
         self._developer = False
+        self._camera = False
+
+    @property
+    def camera(self) -> bool:
+        return self._camera
+
+    @camera.setter
+    def camera(self, value: bool) -> None:
+        if not isinstance(value, bool):
+            raise TypeError("Paralox3D Error: modes.camera must be True or False. " f"Received {type(value).__name__}.")
+        self._camera = value
 
     @property
     def developer(self) -> bool:
