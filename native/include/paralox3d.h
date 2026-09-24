d0078ec1f7f23b34925baab4861326a2471a1d1a
N0one123 <ayaansharma1714@gmail.com>
2026-09-24 17:10:55 +0530

    Add camera C API

--- native/include/paralox3d.h
+++ native/include/paralox3d.h
@@ -23,6 +23,8 @@ P3D_API void p3d_engine_destroy(P3DEngine* engine);
 P3D_API uint32_t p3d_entity_create(P3DEngine* engine);
 P3D_API void p3d_entity_set_position(P3DEngine* engine, uint32_t entity, float x, float y, float z);
 P3D_API int p3d_input_key_held(P3DEngine* engine, int key_code);
+P3D_API void p3d_camera_set_enabled(P3DEngine* engine, int enabled);
+P3D_API void p3d_camera_set_transform(P3DEngine* engine, float x, float y, float z, float pitch, float yaw, float roll);
 P3D_API void p3d_engine_set_developer_overlay(P3DEngine* engine, int enabled, int object_count,
                                               float fps, const char* current_task, int warning_count);
 P3D_API int p3d_engine_diagnostics_clicked(P3DEngine* engine);
