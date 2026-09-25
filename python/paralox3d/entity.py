c3a0bda33e3a2f078bc251025320bb1965c4538d
N0one123 <ayaansharma1714@gmail.com>
2026-09-25 17:59:03 +0530

    Complete Object native state and QoL helpers

--- python/paralox3d/entity.py
+++ python/paralox3d/entity.py
@@ -28,9 +28,9 @@ class Object:
         target=scene or current_scene()
         if target is not None:target.add(self)
     def _push(self):
-        self._engine._set_position(self._handle,self._position); self._engine._set_scale(self._handle,self._scale)
+        self._engine._set_position(self._handle,self._position); self._engine._set_rotation(self._handle,self._rotation); self._engine._set_scale(self._handle,self._scale)
         if hasattr(self._engine,"_set_enabled"):self._engine._set_enabled(self._handle,self.enabled,self.visible)
-        if hasattr(self._engine,"_set_color"):self._engine._set_color(self._handle,*self.color)
+        if hasattr(self._engine,"_set_color"):self._engine._set_color(self._handle,*self.color,self.opacity)
     @property
     def model(self):return self._model
     @property
@@ -89,7 +89,7 @@ class Object:
         self.parent=parent
         if parent and self not in parent._children:parent._children.append(self)
         self._recompute_world();return self
-    def detach(self):return self.set_parent(None)
+    def detach(self):return self.set_parent(None)\n    @property\n    def children(self):return tuple(self._children)\n    @property\n    def components(self):return tuple(self._components)\n    def add_tag(self,tag):self.tags.add(str(tag));return self\n    def remove_tag(self,tag):self.tags.discard(str(tag));return self\n    def has_tag(self,tag):return str(tag) in self.tags
     def move(self,x=0,y=0,z=0):return self.translate(x,y,z)
     def translate(self,x=0,y=0,z=0):self._local_position=self._local_position+Vec3(x,y,z);self._recompute_world();return self
     def rotate(self,x=0,y=0,z=0):self._local_rotation=self._local_rotation+Vec3(x,y,z);self._recompute_world();return self
